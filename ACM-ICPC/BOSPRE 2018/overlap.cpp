f6ccc1260a5194fbe5ebe6d7374546259ac02674
Noble-Mushtak <noblemushtak.public@gmail.com>
2020-08-08 18:04:47 -0400

    Finished overlap solution

--- ACM-ICPC/BOSPRE 2018/overlap.cpp
+++ ACM-ICPC/BOSPRE 2018/overlap.cpp
@@ -12,7 +12,61 @@
 #define DEBUG(...)
 #endif
 
-#include "../../Libraries/struct_wavelet_tree.h"
+namespace wavelet_tree {
+    typedef int64_t num_items;
+    
+    template<class num> struct tree {
+        num minVal, maxVal;
+        tree *leftChild, *rightChild;
+        //itemsInLeftChild[i] denotes how many elements from arr[0..i] are in the leftChild of this wavelet tree
+        //NOTE: arr[0..i] does not include arr[i]
+        std::vector<num_items> itemsInLeftChild;
+
+        //Construct a wavelet tree to represent the elements [beginIt, endIt), where every element is in the interval [minVal, maxVal]          
+        template<class num_it> tree(num_it beginIt, num_it endIt, num minV, num maxV) : minVal(minV), maxVal(maxV), leftChild(nullptr), rightChild(nullptr) {
+            //If this node represents a bunch of items which have all the same value or this node represents an empty subarray, then this node is a leaf of the wavelet tree.
+            if ((minVal == maxVal) || (beginIt >= endIt)) { 
+                leftChild = nullptr;
+                rightChild = nullptr;
+                return;
+            }
+            
+            itemsInLeftChild.reserve(endIt-beginIt+1);
+            //Here, itemsInLeftChild[0] denotes the number of elements from beginIt[0..0] which are in the leftChild, which is obviously 0:
+            itemsInLeftChild.push_back(0);
+            
+            num midVal = (minVal+maxVal)/2;
+            for (num_it it = beginIt; it != endIt; ++it) {
+                //Add 1 to this element in itemsInLeftChild if *it <= mid, i.e. if this element is going to be put in the left child
+                itemsInLeftChild.push_back(itemsInLeftChild.back() + (*it <= midVal));
+            }
+
+            //Partition the elements so that all elements <= mid are on the left side and all elements > mid are on the right side
+            //pivotIt points to the first element on the right side
+            num_it pivotIt = std::stable_partition(beginIt, endIt, [midVal](num x) { return x <= midVal; });
+            
+            leftChild = new tree(beginIt, pivotIt, minVal, midVal);
+            rightChild = new tree(pivotIt, endIt, midVal+1, maxVal);
+        }
+
+        //Count number of elements in arr[leftIndex..rightIndex] in the interval [queryMin, queryMax]
+        num_items withinInterval(num_items leftIndex, num_items rightIndex, num queryMin, num queryMax) {
+            //If this range of elements is empty or there is no overlap, return 0:
+            if ((leftIndex >= rightIndex) || (queryMax < queryMin) || (queryMax < minVal) || (maxVal < queryMin)) return 0;
+            //If there is only total overlap, return all elements:
+            if ((queryMin <= minVal) && (maxVal <= queryMax)) return rightIndex-leftIndex;
+
+            num_items leftIndexOfLeftChild = itemsInLeftChild[leftIndex];
+            num_items rightIndexOfLeftChild = itemsInLeftChild[rightIndex];
+            
+            return leftChild->withinInterval(
+                leftIndexOfLeftChild, rightIndexOfLeftChild, queryMin, queryMax
+            )+rightChild->withinInterval(
+                leftIndex-leftIndexOfLeftChild, rightIndex-rightIndexOfLeftChild, queryMin, queryMax
+            );
+        }
+    };
+}
 
 typedef int64_t num_rects;
 typedef int64_t coord;
@@ -47,15 +101,33 @@ bool operator<(labeled_coord lc1, labeled_coord lc2) { return lc1.loc < lc2.loc;
 
 const num_rects MAX_RECTS = 1000010;
 char testCaseName[100];
-num_rects numRects, minXInds[MAX_RECTS], maxXInds[MAX_RECTS], minYInds[MAX_RECTS], maxYInds[MAX_RECTS], cornerYs[MAX_RECTS];
+num_rects numRects, minXInds[MAX_RECTS], maxXInds[MAX_RECTS], minYInds[MAX_RECTS], maxYInds[MAX_RECTS], cornerYs[MAX_RECTS], inverseXInds[MAX_RECTS];
 rect rects[MAX_RECTS];
-labeled_coord minXs[MAX_RECTS], maxXs[MAX_RECTS];
-coord minYs[MAX_RECTS], maxYs[MAX_RECTS];
+labeled_coord minXs[MAX_RECTS], maxXs[MAX_RECTS], minYs[MAX_RECTS], maxYs[MAX_RECTS];
 
-void buildCornerYs(labeled_coord *xs, coord_type cType, coord *ys) {
+void calcInds(labeled_coord *origCoords, labeled_coord *refCoords, num_rects *inds, bool upperBound) {
+    num_rects origIdx = 0, refIdx;
+    REP(refIdx, numRects) {
+        while ((origIdx < numRects) && (upperBound ? (refCoords[refIdx].loc > origCoords[origIdx].loc) : (refCoords[refIdx].loc >= origCoords[origIdx].loc))) {
+            inds[origCoords[origIdx].ind] = refIdx;
+            ++origIdx;
+        }
+    }
+    for (; origIdx < numRects; ++origIdx) inds[origCoords[origIdx].ind] = numRects;
+}
+
+void calcInverseInds(labeled_coord *arr, num_rects *inverseInds) {
     num_rects i;
-    REP(i, numRects) {
-        cornerYs[i] = std::lower_bound(ys, ys+numRects, rects[xs[i].ind].getCoord(cType))-ys;
+    REP(i, numRects) inverseInds[arr[i].ind] = i;
+}
+
+void buildCornerYs(num_rects *inverseXInds, labeled_coord *ys) {
+    num_rects xsIdx, ysIdx = 0;
+    while (ysIdx < numRects) {
+        num_rects lastYsIdx = ysIdx;
+        for (; (ysIdx < numRects) && (ys[ysIdx].loc == ys[lastYsIdx].loc); ++ysIdx) cornerYs[inverseXInds[ys[ysIdx].ind]] = lastYsIdx;
+        
+        //cornerYs[i] = std::lower_bound(ys, ys+numRects, labeled_coord(rects[xs[i].ind].getCoord(cType), 0))-ys;
     }
 }
 
@@ -79,84 +151,80 @@ int main() {
             curLineStream >> rects[numRects].minX >> rects[numRects].maxX >> rects[numRects].minY >> rects[numRects].maxY;
             minXs[numRects] = labeled_coord(rects[numRects].minX, numRects);
             maxXs[numRects] = labeled_coord(rects[numRects].maxX, numRects);
-            minYs[numRects] = rects[numRects].minY;
-            maxYs[numRects] = rects[numRects].maxY;
+            minYs[numRects] = labeled_coord(rects[numRects].minY, numRects);
+            maxYs[numRects] = labeled_coord(rects[numRects].maxY, numRects);
             ++numRects;
         }
         
-        num_rects answer = 0, i;
+        num_rects answer = numRects*(numRects-1), i;
         
+        std::sort(minXs, minXs+numRects);
         std::sort(maxXs, maxXs+numRects);
+        calcInds(minXs, maxXs, minXInds, true);
         REP(i, numRects) {
-            answer += numRects-1;
-            
             //maxX <= rects[i].minX
-            minXInds[i] = std::upper_bound(maxXs, maxXs+numRects, labeled_coord(rects[i].minX, 0))-maxXs;
+            //minXInds[i] = std::upper_bound(maxXs, maxXs+numRects, labeled_coord(rects[i].minX, 0))-maxXs;
             answer -= minXInds[i];
         }
         
-        std::sort(minXs, minXs+numRects);
+        calcInds(maxXs, minXs, maxXInds, false);
         REP(i, numRects) {
             //minX >= rects[i].maxX
-            maxXInds[i] = std::lower_bound(minXs, minXs+numRects, labeled_coord(rects[i].maxX, 0))-minXs;
+            //maxXInds[i] = std::lower_bound(minXs, minXs+numRects, labeled_coord(rects[i].maxX, 0))-minXs;
             answer -= numRects-maxXInds[i];
         }
         
+        std::sort(minYs, minYs+numRects);
         std::sort(maxYs, maxYs+numRects);
+        calcInds(minYs, maxYs, minYInds, true);
         REP(i, numRects) {   
             //maxY <= rects[i].minY
-            minYInds[i] = std::upper_bound(maxYs, maxYs+numRects, rects[i].minY)-maxYs;
+            //minYInds[i] = std::upper_bound(maxYs, maxYs+numRects, rects[i].minY)-maxYs;
             answer -= minYInds[i];
         }
         
-        std::sort(minYs, minYs+numRects);
+        calcInds(maxYs, minYs, maxYInds, false);
         REP(i, numRects) {
             //minY >= rects[i].maxY
-            maxYInds[i] = std::lower_bound(minYs, minYs+numRects, rects[i].maxY)-minYs;
+            //maxYInds[i] = std::lower_bound(minYs, minYs+numRects, rects[i].maxY)-minYs;
             answer -= numRects-maxYInds[i];
         }
 
-        buildCornerYs(minXs, coord_type::MIN_Y, minYs);
+        calcInverseInds(minXs, inverseXInds);
+        buildCornerYs(inverseXInds, minYs);
         wavelet_tree::tree<num_rects> topLeftCornersTree(cornerYs, cornerYs+numRects, 0, numRects-1);
-        //wavelet_tree::tree<num_rects> cornersTree(cornerYs, cornerYs+numRects, 0, numRects-1);
         REP(i, numRects) {
             //minX >= rects[i].maxX, minY >= rects[i].maxY
             answer += topLeftCornersTree.withinInterval(maxXInds[i], numRects, maxYInds[i], numRects-1);
-            //answer += cornersTree.withinInterval(maxXInds[i], numRects, maxYInds[i], numRects-1);
         }
         
-        buildCornerYs(maxXs, coord_type::MIN_Y, minYs);
-        wavelet_tree::tree<num_rects> topRightCornersTree(cornerYs, cornerYs+numRects, 0, numRects-1);
-        //cornersTree.reconstruct(cornerYs, cornerYs+numRects, 0, numRects-1);
-        REP(i, numRects) {
-            //maxX <= rects[i].minX, minY >= rects[i].maxY
-            answer += topRightCornersTree.withinInterval(0, minXInds[i], maxYInds[i], numRects-1);
-            //answer += cornersTree.withinInterval(0, minXInds[i], maxYInds[i], numRects-1);
-        }
-        
-        buildCornerYs(minXs, coord_type::MAX_Y, maxYs);
+        buildCornerYs(inverseXInds, maxYs);
         wavelet_tree::tree<num_rects> bottomLeftCornersTree(cornerYs, cornerYs+numRects, 0, numRects-1);
-        //cornersTree.reconstruct(cornerYs, cornerYs+numRects, 0, numRects-1);
         REP(i, numRects) {
             //minX >= rects[i].maxX, maxY <= rects[i].minY
             answer += bottomLeftCornersTree.withinInterval(maxXInds[i], numRects, 0, minYInds[i]-1);
-            //answer += cornersTree.withinInterval(maxXInds[i], numRects, 0, minYInds[i]-1);   
         }
         
-        buildCornerYs(maxXs, coord_type::MAX_Y, maxYs);
+        calcInverseInds(maxXs, inverseXInds);
+        buildCornerYs(inverseXInds, minYs);
+        wavelet_tree::tree<num_rects> topRightCornersTree(cornerYs, cornerYs+numRects, 0, numRects-1);
+        REP(i, numRects) {
+            //maxX <= rects[i].minX, minY >= rects[i].maxY
+            answer += topRightCornersTree.withinInterval(0, minXInds[i], maxYInds[i], numRects-1);
+        }
+        
+        buildCornerYs(inverseXInds, maxYs);
         wavelet_tree::tree<num_rects> bottomRightCornersTree(cornerYs, cornerYs+numRects, 0, numRects-1);
-        //cornersTree.reconstruct(cornerYs, cornerYs+numRects, 0, numRects-1);
         REP(i, numRects) {
             //maxX <= rects[i].minX, maxY <= rects[i].minY
             answer += bottomRightCornersTree.withinInterval(0, minXInds[i], 0, minYInds[i]-1);
-            //answer += cornersTree.withinInterval(0, minXInds[i], 0, minYInds[i]-1);
         }
 
         if (answer & 1) {
             std::cerr << "Odd number of answers: " << answer << "\n";
             exit(1);
         }
-        std::cout << answer/2 << "\n";
+        std::cout << (answer >> 1) << "\n";
     }
 
     return 0;
